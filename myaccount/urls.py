c654822fb843c4fbb4437521a6be6b549af76b9b
manas-anarov <microsoft.kg@mail.ru>
2019-08-27 17:40:22 +0600

    change login

--- myaccount/urls.py
+++ myaccount/urls.py
@@ -2,12 +2,12 @@
 from django.urls import path
 
 from django.conf.urls import include, url
-from django.contrib.auth.views import login, logout;
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-	url(r'^login/$', login, {'template_name': 'myaccount/login.html'}, name='login'),
-	url(r'^logout/$', logout, {'template_name': 'myaccount/logout.html'}, name='logout'),
+	url(r'^login/$', auth_views.LoginView.as_view(template_name='myaccount/login.html'), name='login'),
+	url(r'^logout/$', auth_views.LogoutView.as_view(template_name='myaccount/logout.html'), name='logout'),
 	url(r'^register/$', views.register, name='register'),
 ]
---
+++ readme.txt
@@ -0,0 +1,8 @@
+django-admin.py startproject kungfu_blog
+cd kungfu_blog
+
+python manage.py runserver
+python manage.py migrate
+python manage.py makemigrations
+python manage.py startapp myaccount
+python manage.py createsuperuser
